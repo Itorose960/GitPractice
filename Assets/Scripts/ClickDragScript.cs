8e45f269e5273da9f172aa95335a135c324944a3
Itorose960 <itorose960@g.educaand.es>
2023-02-07 07:59:50 +0100

    first commit

---
+++ Assets/Scripts/ClickDragScript.cs
@@ -0,0 +1,43 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ClickDragScript : MonoBehaviour
+{
+    private GameObject heldObject;
+    private void FixedUpdate()
+    {
+        if(heldObject != null)
+        {
+            Vector3 pos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            pos.z = heldObject.transform.position.z;
+            heldObject.transform.position = pos;
+        }
+    }
+    private void Update()
+    {
+        if(Input.GetMouseButtonDown(0))
+        {
+            GrabItem();
+        }
+        if(Input.GetMouseButtonUp(0))
+        {
+            heldObject = null;
+        }
+    }
+
+    private void GrabItem()
+    {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+        RaycastHit hit;
+
+        if(Physics.Raycast(ray, out hit)) { 
+            if(hit.collider.CompareTag("Draggable"))
+            {
+                heldObject = hit.collider.gameObject;
+            }
+        }
+    }
+}
